59b90fdb85d8d5916e197247975c35c2c771a958
arianasaditotshami-droid <arianasaditotshami@gmail.com>
2026-08-06 15:50:34 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+from database import add_order, get_orders
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -29,3 +30,50 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="خرید کانفیگ")],
+        [KeyboardButton(text="سفارش‌های من")]
+    ],
+    resize_keyboard=True
+)
+
+
+@dp.message(lambda message: message.text == "خرید کانفیگ")
+async def buy(message: Message):
+    await message.answer(
+        "حجم مورد نظر را انتخاب کنید:\n"
+        "1- 3GB\n"
+        "2- 5GB\n"
+        "3- 10GB"
+    )
+
+
+@dp.message(lambda message: message.text in ["3GB", "5GB", "10GB"])
+async def order(message: Message):
+    add_order(
+        message.from_user.id,
+        message.text
+    )
+
+    await message.answer(
+        "سفارش شما ثبت شد ✅"
+    )
+
+
+@dp.message(lambda message: message.text == "سفارش‌های من")
+async def my_orders(message: Message):
+    orders = get_orders(message.from_user.id)
+
+    if not orders:
+        await message.answer("سفارشی ندارید.")
+        return
+
+    text = "سفارش‌های شما:\n"
+
+    for order in orders:
+        text += f"- {order[0]}\n"
+
+    await message.answer(text)
