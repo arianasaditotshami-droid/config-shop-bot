dac41d48bc8cbe9abac3f2084d92eb1ae0c4dd14
arianasaditotshami-droid <arianasaditotshami@gmail.com>
2026-08-06 10:59:10 +0330

    Implement basic Telegram bot with start and echo commands
    Added a basic Telegram bot using aiogram with start and echo commands.

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import asyncio
 
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from configs import BOT_TOKEN
+from db import create_tables
+
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer(
+        "سلام 👋\n"
+        "به ربات فروش کانفینگ خوش آمدید."
+    )
+
+
+@dp.message()
+async def echo(message: Message):
+    await message.answer(
+        "لطفاً از منوی ربات استفاده کنید."
+    )
+
+
+async def main():
+    create_tables()
+
+    print("Bot is running...")
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
